11012f561d7f8804ab798630505ea7ec41a296ed
yonatanm <yonatanm@gmail.com>
2013-09-15 00:19:10 +0300

    fix some bugs

--- angularApp/timeToGo.js
+++ angularApp/timeToGo.js
@@ -3,6 +3,7 @@
 /* App Module */
 
 var mockedAndroidInterface = {};
+var ROOT;
 angular.module('timeToGo.controllers', []);
 angular.module('timeToGo.directives', []);
 angular.module('timeToGo.controllers.mock', []);
@@ -32,47 +33,22 @@ timeToGoApp.config(function($routeProvider ) {
 
   HistoryService.init();
   
-$rootScope.safeApply = function(fn) {
-  var phase = this.$root.$$phase;
-  if(phase == '$apply' || phase == '$digest') {
-    if(fn && (typeof(fn) === 'function')) {
-      fn();
+  $rootScope.safeApply = function(fn) {
+    var phase = this.$root.$$phase;
+    if(phase == '$apply' || phase == '$digest') {
+      if(fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
     }
-  } else {
-    this.$apply(fn);
-  }
-};
+  };
+
+ ROOT = $rootScope;
+});
 
 
-
-
- // $rootScope.fromAndroiad = fromAndroiad;
-
-
-    // onCreate: function() {
-    //     console.log("in onCreate");
-    //     $rootScope.waitingForLocation = false;
-    // },
-    // onStart: function() {},
-    // onResume: function() {},
-    // onPause: function() {},
-    // updateUI: function(travelTime, routeName, updateTime) {},
-    // onTimeToGo: function(travelTime, routeName, updateTime) {
-    //   $rootScope.go = {
-    //     travelTime: travelTime,
-    //     routeName: routeName,
-    //     arivalTime: moment().add(travelTime, "minutes").format("hh:mm")
-    //   }
-    //   $location.path("/timeToGo");
-    // }, 
-    // onDrivingTime: function() {},
-    // onCurrentLocation: function(lat, long) {
-    //   console.log("in onCurrentLocation");
-    //   $rootScope.waitingForLocation = false;
-    // },
-  // };
-
-  Application = {
+var Application = {
     onCreate: function() {
         console.log("on onCreate");
     },
@@ -87,25 +63,23 @@ $rootScope.safeApply = function(fn) {
     },
     updateUI: function(maxDrivingTime, routeName, lastUpdated) {
       console.log(sprintf("on updateUI with maxDrivingTime=%(maxDrivingTime)s routeName=%(routeName)s", 
-        maxDrivingTime, routeName);
+        maxDrivingTime, routeName));
     },
     onTimeToGo: function(maxDrivingTime, routeName, lastUpdated) {
       console.log(sprintf("on onTimeToGo with maxDrivingTime=%(maxDrivingTime)s routeName=%(routeName)s", 
-        maxDrivingTime, routeName);
+        maxDrivingTime, routeName));
     },
     onDrivingTime: function(maxDrivingTime) {
-      console.log(sprintf("on onDrivingTime with maxDrivingTime = %(maxDrivingTime)s", maxDrivingTime);
+      console.log(sprintf("on onDrivingTime with maxDrivingTime = %(maxDrivingTime)s", maxDrivingTime));
     },
     onCurrentLocation: function(geoLocation) {
 
       console.log("onCurrentLocation "+geoLocation.lat+" / "+geoLocation.lng);
-      $rootScope.safeApply(function(){ 
-        $rootScope.currentLocation = geoLocation; 
+      ROOT.safeApply(function(){ 
+        ROOT.currentLocation = geoLocation; 
       });
     }
   };
-});
-
 
 
 
