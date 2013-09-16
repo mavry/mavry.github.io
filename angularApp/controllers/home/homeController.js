4a70db39f4c7cd1e3f947aa8c5120be70d4ae8c5
yonatanm <yonatanm@gmail.com>
2013-09-16 08:39:58 +0300

    add periodic

--- angularApp/controllers/home/homeController.js
+++ angularApp/controllers/home/homeController.js
@@ -16,7 +16,7 @@ angular.module('timeToGo.controllers'). controller('HomeCtrl',  function ($scope
 	  	console.log("got location: "+locAsStrng)
 	  	var currentLocation = $.parseJSON(locAsStrng);
 	  	$scope.onCurrentLocation(currentLocation);
-	  	// $timeout($scope.pollForLocation, 1000);
+	  	$timeout($scope.pollForLocation, 1000);
 	  }
 	};
 
